3f111f173d26aa8394e684344ec2e1a7b91b370e
salahelfatimi <salahfatimi16@gmail.com>
2025-04-14 14:39:07 +0100

    Refactor layout for appointment page: adjust height and image styling for better responsiveness

--- src/app/prendre-rendez-vous/page.jsx
+++ src/app/prendre-rendez-vous/page.jsx
@@ -4,8 +4,8 @@ import Image from "next/image";
 export default function Prendre_rendez_vous(){
     return(
         <div >
-            <div className="h-screen relative ">
-                <Image src={'/img/prendre-rendez-vous/img.jpg'} alt="FAQ" fill className="object-cover object-center" />
+            <div className="min-h-screen relative ">
+                <Image src={'/img/prendre-rendez-vous/img.jpg'} alt="FAQ" fill className="object-cover object-center h-full w-full" />
                 <div className="absolute top-0 left-0 w-full h-full bg-black opacity-50"></div>
                 <Prendre_rendez_vous_page/>
             </div>
